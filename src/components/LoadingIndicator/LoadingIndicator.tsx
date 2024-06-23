80293e5b2025908af631c61a2ef3702015d783cc
Niclas Steidle <niclassteidle@pflegix.de>
2024-06-23 19:59:59 +0200

    spinner actually should spin

--- src/components/LoadingIndicator/LoadingIndicator.tsx
+++ src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -1,11 +1,10 @@
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import "./LoadingIndicator.css";
 
 type LoadingIndicatorProps = {
   size?: string | number;
 };
 
 export function LoadingIndicator({ size }: LoadingIndicatorProps) {
-  return (
-    <AiOutlineLoading3Quarters className="text-flixcheck-green" size={size} style={{ transform: "rotate(90deg)" }} />
-  );
+  return <AiOutlineLoading3Quarters className="text-flixcheck-green spinner" size={size} />;
 }
