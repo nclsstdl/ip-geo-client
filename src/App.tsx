5968266de8a530fa413ac74486e68336ad352b0e
Niclas Steidle <niclassteidle@pflegix.de>
2024-06-23 07:09:12 +0200

    add tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { IpLookUpForm } from "./components/IpLookUpForm/IpLookUpForm";
 import { IpLookUpResult } from "./components/IpLookUpResult/IpLookUpResult";
 import { ApplicationStateProvider } from "./providers/ContextProvider";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <ApplicationStateProvider>
-      <header className="sticky top-0 flex items-center p-2 shadow-md font-bold text-lg tracking-tighter">
-        Ip Geo Lookup
-      </header>
-      <main className="flex flex-col items-center justify-center">
-        <IpLookUpForm />
-        <IpLookUpResult className="mt-4" />
-      </main>
+      <QueryClientProvider client={queryClient}>
+        <header className="sticky top-0 flex items-center p-2 shadow-md font-bold text-lg tracking-tighter">
+          Ip Geo Lookup
+        </header>
+        <main className="flex flex-col items-center justify-center">
+          <IpLookUpForm />
+          <IpLookUpResult className="mt-4" />
+        </main>
+      </QueryClientProvider>
     </ApplicationStateProvider>
   );
 }
